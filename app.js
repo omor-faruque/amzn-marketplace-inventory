876a81cb23d6fbcedcd682c1dc84f102a433c724
MdOmor Faruque <mdomor.faruque@rci.rogers.com>
2024-02-09 15:27:58 -0500

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
 
-// Your Node.js application code goes here
 console.log('Hello, Node.js!');
+
+
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// URL of the webpage containing the product price
+const url = 'https://www.walmart.ca/en/ip/goody-ouchless-no-metal-elastics-for-thin-hair-brown-36-ct-brown/6000187961048';
+
+window.open(url);
